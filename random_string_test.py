cc7cb8010956ba6e658da793d9335c0f088b3195
Hanbyeol Baek <create6436@naver.com>
2023-08-16 17:06:50 +0900

    테스트 파일 수정

--- random_string_test.py
+++ random_string_test.py
@@ -1,26 +1,6 @@
 import random_string
 import random
 
-# def test_random_string1():
-#     n = random.randint(1, 100)
-#     assert len(random_string.random_string1(n)) == n
-
-# def test_random_string2():
-#     n = random.randint(1, 100)
-#     assert len(random_string.random_string2(n)) == n
-
-# def test_random_string3():
-#     n = random.randint(1, 100)
-#     assert len(random_string.random_string3(n)) == n
-
-# def test_random_string4():
-#     n = random.randint(1, 100)
-#     assert len(random_string.random_string4(n)) == n
-
-# def test_random_string5():
-#     n = random.randint(1, 100)
-#     assert len(random_string.random_string5(n)) == n
-
 def test_random_string():
     n = random.randint(1, 100)
     candidates = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789=- ")
@@ -42,10 +22,3 @@ def test_random_string():
     # n = 1로 테스트
     single_char_result = random_string.random_string(1, candidates)
     assert len(single_char_result) == 1
-
-    print("test_random_string passed")
-
-
-if __name__ == "__main__":
-    for _ in range(100):
-        test_random_string()
