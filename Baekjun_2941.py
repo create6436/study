374c7aa73a0d688106a544dd5963858f765e669e
Hanbyeol Baek <create6436@naver.com>
2023-08-17 15:55:58 +0900

    인코딩 테이블생성 함수

--- Baekjun_2941.py
+++ Baekjun_2941.py
@@ -1,13 +1,15 @@
 import random_string
+import EncodingTable
 
-def count_croatian_alphabet_1(word):
-    croatian_alphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "y="]
+encoding_table = EncodingTable.make_encodingTable()
+
+def count_croatian_alphabet_1(word, encoding_table):
     count = 0
     i = 0
 
     while i < len(word):
         found = False
-        for alphabet in croatian_alphabet:
+        for alphabet in encoding_table:
             if word[i:i+len(alphabet)] == alphabet:
                 count += 1
                 i += len(alphabet)
@@ -22,20 +24,17 @@ def count_croatian_alphabet_1(word):
 
 
 # count_croatian_alphabet_2 = lambda word: len(word) - sum([word.count(alphabet) for alphabet in ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]])
-encoding_table = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=", "x="]
-
-def count_croatian_alphabet_2(word):
+def count_croatian_alphabet_2(word, encoding_table):
     cnt = sum([word.count(alphabet) * (len(alphabet) - 1) for alphabet in encoding_table])
     return len(word) - cnt
 
 
 # 크로아티아 알파벳을 찾는 함수 count_croatian_alphabet_3()을 구현하기
 # 단 반복문, 람다식, 내장함수를 사용하지 않고 구현하기
-def count_croatian_alphabet_3(word):
-    croatian_alphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "y="]
+def count_croatian_alphabet_3(word, encoding_table):
     count = 0    
 
-    for alphabet in croatian_alphabet:
+    for alphabet in encoding_table:
         count += word.count(alphabet)
         word = word.replace(alphabet, "▁")
 
@@ -43,11 +42,10 @@ def count_croatian_alphabet_3(word):
     return count
 
 
-def count_croatian_alphabet_4(word):
-    croatian_alphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "y="]
+def count_croatian_alphabet_4(word, encoding_table):
     count = 0    
 
-    for alphabet in croatian_alphabet:
+    for alphabet in encoding_table:
         count += word.count(alphabet)
         word = word.replace(alphabet, "▁")
 
@@ -56,10 +54,8 @@ def count_croatian_alphabet_4(word):
 
 
 if __name__ == "__main__":
-    import random_string
-
     m = int(input("문자열의 길이를 입력하세요:  "))
     word = random_string.random_string(m, list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789=- ") + ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "y="])
     print(word)
-    result = count_croatian_alphabet_3(word)
+    result = count_croatian_alphabet_3(word, encoding_table)
     print("코르아티아 알파벳을 포함한 문자열의 길이:", result)
--- Baekjun_2941_test.py
+++ Baekjun_2941_test.py
@@ -1,11 +1,14 @@
 from Baekjun_2941 import count_croatian_alphabet_1, count_croatian_alphabet_2, count_croatian_alphabet_3
 from random_string import random_string
+from EncodingTable import make_encodingTable
+
+encoding_table = make_encodingTable()
 
 def test_count_croatian_alphabet_1():
     test_cases = [(random_string(10, list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789=- ") + ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "y="]), 10)]
 
     for word, answer in test_cases:
-        my_answer = count_croatian_alphabet_1(word)
+        my_answer = count_croatian_alphabet_1(word, encoding_table)
         assert my_answer == answer, f"Wrong answer. my_answer: {my_answer}, answer: {answer}"
 
 
@@ -13,7 +16,7 @@ def test_count_croatian_alphabet_2():
     test_cases = [(random_string(10, list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789=- ") + ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=", "x="]), 10)]
     
     for word, answer in test_cases:
-        my_answer = count_croatian_alphabet_2(word)
+        my_answer = count_croatian_alphabet_2(word, encoding_table)
         assert my_answer == answer, f"Wrong answer. my_answer: {my_answer}, answer: {answer}"
 
 
@@ -21,7 +24,7 @@ def test_count_croatian_alphabet_3():
     test_cases = [(random_string(10, list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789=- ") + ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "y="]), 10)]
 
     for word, answer in test_cases:
-        my_answer = count_croatian_alphabet_3(word)
+        my_answer = count_croatian_alphabet_3(word, encoding_table)
         assert my_answer == answer, f"Wrong answer. my_answer: {my_answer}, answer: {answer}"
 
 
