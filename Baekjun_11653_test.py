fb56f032b5226dd73e15abcd55ddee248784d58a
Hanbyeol Baek <create6436@naver.com>
2023-08-21 15:38:10 +0900

    소인수분해 함수 테스트함수 수정

--- Baekjun_11653_test.py
+++ Baekjun_11653_test.py
@@ -1,5 +1,6 @@
 from Baekjun_11653 import factorization
 from random import randint
+from prime_util import is_prime
 
 #랜덤으로 소인수분해할 숫자 생성
 def random_number():
@@ -7,7 +8,7 @@ def random_number():
 
 #소인수분해 함수 테스트 코드
 def test_factorization():
-    test_cases = [random_number() for _ in range(10)]
+    test_cases = [random_number() for _ in range(30)]
 
     for test_case in test_cases:
         my_answer = factorization(test_case)  # factorization 함수 호출하여 소인수분해 결과 저장
@@ -18,7 +19,11 @@ def test_factorization():
 
         # 소인수분해 결과를 합친 값과 생성된 숫자를 비교하여 테스트
         assert combined_result == test_case, f"Wrong answer. my_answer: {my_answer}, answer: {test_case}"
-        
+
+        # 소수가 맞는지 검사
+        for factor in my_answer:
+            assert is_prime(factor), f"{factor} is not a prime number."
+
         print("test_case:", test_case, "/ my_answer:", my_answer)
 
 
---
+++ prime_util.py
@@ -0,0 +1,15 @@
+# 주어진 숫자가 소수인지 검사하는 함수
+def is_prime(num):
+    i = 2
+    while i * i <= num:  # i가 num의 제곱근보다 작거나 같을 때까지 반복
+        if num % i == 0 or num % (i + 2) == 0:
+            assert False, f"{num} is not a prime number."
+        i += 1
+    return True   # 위의 조건들을 모두 통과하면 소수로 간주
+
+if __name__ == "__main__":
+    num = int(input("소수인지 검사할 숫자:"))
+    if is_prime(num):
+        print("소수입니다.")
+    else:
+        print("소수가 아닙니다.")
