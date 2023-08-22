76c21a45b16d96913169ec4532aa88a2e4065998
Hanbyeol Baek <create6436@naver.com>
2023-08-22 14:37:10 +0900

    4949번 테스트케이스 추가

--- Baekjun_4949_test.py
+++ Baekjun_4949_test.py
@@ -7,6 +7,10 @@ def test_is_balanced():
     assert is_balanced("({[]})") == True
     assert is_balanced("[{()}]") == True
     assert is_balanced("(([]))") == True
+    assert is_balanced(" ") == True
+    assert is_balanced("( ) o ( )") == True
+    assert is_balanced("hello") == True
+    assert is_balanced("hello world") == True
 
     # 균형이 깨진 경우
     assert is_balanced("(") == False
@@ -22,6 +26,8 @@ def test_is_balanced():
     assert is_balanced(")(") == False
     assert is_balanced("[[[]]]]") == False
     assert is_balanced("[[[[[[[]]]]") == False
+    assert is_balanced("hello world(") == False
+    assert is_balanced("hello world)") == False
 
 if __name__ == "__main__":
     test_is_balanced()
