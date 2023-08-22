b3468a77568145f2a6520b2852248a60237423cb
Hanbyeol Baek <create6436@naver.com>
2023-08-22 13:54:27 +0900

    딕셔너리 형변환중 생긴 오류 수정

--- Baekjun_11653_test.py
+++ Baekjun_11653_test.py
@@ -6,22 +6,25 @@ from prime_util import is_prime
 def random_number():
     return randint(1, 10000000)
 
-#소인수분해 함수 테스트 코드
 def test_factorization():
     test_cases = [random_number() for _ in range(30)]
 
     for test_case in test_cases:
         my_answer = factorization(test_case)  # factorization 함수 호출하여 소인수분해 결과 저장
 
-        combined_result = 1  # 소인수분해 결과를 합치기 위한 변수 초기화
-        for factor in my_answer:
-            combined_result *= factor  # 소인수분해 결과를 하나씩 곱하여 합치기
+        reconstructed_result = 1  # 소인수분해 결과를 다시 계산할 변수 초기화
+        for factor, count in my_answer.items():  # 소수인수와 그 개수를 반복
+            reconstructed_result *= factor ** count  # 소수인수와 그 개수를 이용하여 결과 계산하기
 
-        # 소인수분해 결과를 합친 값과 생성된 숫자를 비교하여 테스트
-        assert combined_result == test_case, f"Wrong answer. my_answer: {my_answer}, answer: {test_case}"
+        # 소인수분해 결과를 다시 계산한 값과 원래 숫자를 비교하여 테스트
+        assert reconstructed_result == test_case, f"Wrong answer. my_answer: {my_answer}, answer: {test_case}"
 
         # 소수가 맞는지 검사
-        for factor in my_answer:
+        for factor in my_answer.keys():  # 소수인수만 검사
             assert is_prime(factor), f"{factor} is not a prime number."
 
         print("test_case:", test_case, "/ my_answer:", my_answer)
+
+if __name__ == "__main__":
+    test_factorization()
+    print("Everything passed")
--- prime_util.py
+++ prime_util.py
@@ -3,12 +3,20 @@ def is_prime(num):
     if num < 2:  # 2보다 작은 숫자는 소수가 아님
         return False
 
-    i = 2
+    if num == 2:  # 2는 소수
+        return True
+
+    if num % 2 == 0:  # 짝수는 소수가 아님 (2를 제외하고 모든 짝수는 소수가 아님)
+        return False
+
+    i = 3
     while i * i <= num:  # i가 num의 제곱근보다 작거나 같을 때까지 반복
-        if num % i == 0 or num % (i + 2) == 0:
-            assert False, f"{num} is not a prime number."
-        i += 1
-    return True   # 위의 조건들을 모두 통과하면 소수로 간주
+        if num % i == 0:  # num이 i로 나누어 떨어지면 소수가 아님
+            return False
+        i += 2  # 홀수만을 대상으로 반복 검사 (짝수는 위에서 걸러짐)
+
+    return True  # 위의 조건들을 모두 통과하면 소수로 간주
+
 
 if __name__ == "__main__":
     num = int(input("소수인지 검사할 숫자:"))
